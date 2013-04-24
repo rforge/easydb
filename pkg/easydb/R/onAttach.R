f41a690bdba2e077642f31ea7f56ea193b42b687
jmoeys <jmoeys@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-04-24 12:19:06 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/easydb@50 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/easydb/R/onAttach.R
@@ -0,0 +1,45 @@
+# .onLoad <- function(# Print a welcome message
+# ### Print a welcome message.
+
+ # libname, 
+# ### See help(".onLoad")
+
+ # pkgname
+# ### See help(".onLoad")
+
+# ){  #
+    # msg <- sprintf( 
+        # "'%s' package loaded. Type help(package='%s') for examples and help.", 
+        # pkgname, pkgname ) 
+    
+    # msg2 <- sprintf( 
+        # "    Notice that '%s' has been significantly refactored. Please have a look at 'easyrsqlite', 'easyrodbcaccess', 'easyrodbcexcel' or 'easyrodbcmysql'.", 
+        # pkgname, pkgname ) 
+    
+    # packageStartupMessage( msg, msg2 ) 
+# ### Does not return anything.
+# }   #
+
+
+
+.onAttach <- function(# Print a welcome message
+### Print a welcome message.
+
+ libname, 
+### See help(".onLoad")
+
+ pkgname
+### See help(".onLoad")
+
+){  #
+    msg <- sprintf( 
+        "'%s' package loaded. Type help(package='%s') for examples and help.", 
+        pkgname, pkgname ) 
+    
+    # msg2 <- sprintf( 
+        # "    Notice that '%s' has been significantly refactored. Please have a look at 'easyrsqlite', 'easyrodbcaccess', 'easyrodbcexcel' or 'easyrodbcmysql'.", 
+        # pkgname, pkgname ) 
+    
+    packageStartupMessage( msg ) # , msg2
+### Does not return anything.
+}   #
